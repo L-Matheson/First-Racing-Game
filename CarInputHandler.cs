c4f6f77763e524e0947a5fbed3f0da2d0a607310
lmatheson2026 <144724348+lmatheson2026@users.noreply.github.com>
2024-11-10 18:04:48 -0500

    Created car handlers

---
+++ CarInputHandler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CarInputHandler : MonoBehaviour
+{
+    TopDownCarController topDownCarController;
+
+    void Awake() {
+        topDownCarController = GetComponent<TopDownCarController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+
+        topDownCarController.SetInputVector(inputVector);
+    }
+}
